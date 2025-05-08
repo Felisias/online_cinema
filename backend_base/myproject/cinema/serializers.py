05c05064d74a9355de9542ebf2c576524d5fd443
Felisias <knd04@internet.ru>
2025-05-08 11:53:49 +0400

    ЛР_2_3_(step_views)

--- backend_base/myproject/cinema/serializers.py
+++ backend_base/myproject/cinema/serializers.py
@@ -1,7 +1,55 @@
 from rest_framework import serializers
-from .models import Movie
+from .models import (
+    User,
+    Genre,
+    Content,
+    Movie,
+    Series,
+    Season,
+    Episode
+)
 
-class FilmSerializer(serializers.ModelSerializer):
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['id', 'email', 'username', 'first_name', 'last_name']
+
+class GenreSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Genre
+        fields = '__all__'
+
+class ContentSerializer(serializers.ModelSerializer):
+    genre = GenreSerializer()
+
+    class Meta:
+        model = Content
+        fields = '__all__'
+
+class MovieSerializer(serializers.ModelSerializer):
+    content = ContentSerializer()
+
     class Meta:
         model = Movie
+        fields = '__all__'
+
+class SeriesSerializer(serializers.ModelSerializer):
+    content = ContentSerializer()
+
+    class Meta:
+        model = Series
+        fields = '__all__'
+
+class SeasonSerializer(serializers.ModelSerializer):
+    series = SeriesSerializer()
+
+    class Meta:
+        model = Season
+        fields = '__all__'
+
+class EpisodeSerializer(serializers.ModelSerializer):
+    season = SeasonSerializer()
+
+    class Meta:
+        model = Episode
         fields = '__all__'
--- backend_base/myproject/cinema/views.py
+++ backend_base/myproject/cinema/views.py
@@ -1,54 +1,62 @@
-from django.shortcuts import render
-from django.views import View
+from rest_framework import viewsets
+from .models import (
+    User,
+    Genre,
+    Content,
+    Movie,
+    Series,
+    Season,
+    Episode
+)
+from .serializers import (
+    UserSerializer,
+    GenreSerializer,
+    ContentSerializer,
+    MovieSerializer,
+    SeriesSerializer,
+    SeasonSerializer,
+    EpisodeSerializer
+)
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
 
-# Create your views here.
+
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    permission_classes = [IsAuthenticatedOrReadOnly]
 
 
-from rest_framework import status, generics
-from rest_framework.response import Response
-from rest_framework.decorators import api_view
-from .models import Movie
-#from .serializers import MovieSerializer
-from .serializers import FilmSerializer
+class GenreViewSet(viewsets.ModelViewSet):
+    queryset = Genre.objects.all()
+    serializer_class = GenreSerializer
+    permission_classes = [IsAuthenticatedOrReadOnly]
 
 
+class ContentViewSet(viewsets.ModelViewSet):
+    queryset = Content.objects.all()
+    serializer_class = ContentSerializer
+    permission_classes = [IsAuthenticatedOrReadOnly]
 
-class FilmListCreateView(generics.ListCreateAPIView):
+
+class MovieViewSet(viewsets.ModelViewSet):
     queryset = Movie.objects.all()
-    serializer_class = FilmSerializer
-
-class FilmRetrieveUpdateDeleteView(generics.RetrieveUpdateDestroyAPIView):
-    queryset = Movie.objects.all()
-    serializer_class = FilmSerializer
+    serializer_class = MovieSerializer
+    permission_classes = [IsAuthenticatedOrReadOnly]
 
 
+class SeriesViewSet(viewsets.ModelViewSet):
+    queryset = Series.objects.all()
+    serializer_class = SeriesSerializer
+    permission_classes = [IsAuthenticatedOrReadOnly]
 
 
-#@api_view(['POST'])
-#def add_movie(request):
-#    if request.method == 'POST':
-#        serializer = MovieSerializer(data=request.data)
-#        if serializer.is_valid():
-#            serializer.save()
-#            return Response(serializer.data, status=status.HTTP_201_CREATED)
-#        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+class SeasonViewSet(viewsets.ModelViewSet):
+    queryset = Season.objects.all()
+    serializer_class = SeasonSerializer
+    permission_classes = [IsAuthenticatedOrReadOnly]
 
 
-# Пример GET метода
-#class FilmListView(View):
-#    def get(self, request):
-#        films = Film.objects.all()  # Получаем все фильмы
-#        films_data = [{"title": film.title, "genre": film.genre} for film in films]
-#        return JsonResponse(films_data, safe=False)
-#
-## Пример POST метода
-#@csrf_exempt  # Это отключит проверку CSRF для этого метода (если это необходимо)
-#class FilmCreateView(View):
-#    def post(self, request):
-#        data = json.loads(request.body)  # Чтение данных из тела запроса
-#        film = Film.objects.create(
-#            title=data["title"],
-#            genre=data["genre"],
-#            release_year=data["release_year"]
-#        )
-#        return JsonResponse({"id": film.id}, status=201)
+class EpisodeViewSet(viewsets.ModelViewSet):
+    queryset = Episode.objects.all()
+    serializer_class = EpisodeSerializer
+    permission_classes = [IsAuthenticatedOrReadOnly]
