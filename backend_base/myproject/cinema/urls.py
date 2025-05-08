20d17e1bc57fb727c5b8c9db16570cd635e12cc8
Felisias <knd04@internet.ru>
2025-05-08 13:08:17 +0400

    ЛР_2_5_(step_postmen)

--- backend_base/myproject/cinema/urls.py
+++ backend_base/myproject/cinema/urls.py
@@ -1,7 +1,24 @@
-from django.urls import path
-from .views import FilmListCreateView, FilmRetrieveUpdateDeleteView
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .views import (
+    UserViewSet,
+    GenreViewSet,
+    ContentViewSet,
+    MovieViewSet,
+    SeriesViewSet,
+    SeasonViewSet,
+    EpisodeViewSet
+)
+
+router = DefaultRouter()
+router.register(r'users', UserViewSet)
+router.register(r'genres', GenreViewSet)
+router.register(r'contents', ContentViewSet)
+router.register(r'movies', MovieViewSet)
+router.register(r'series', SeriesViewSet)
+router.register(r'seasons', SeasonViewSet)
+router.register(r'episodes', EpisodeViewSet)
 
 urlpatterns = [
-    path('films/', FilmListCreateView.as_view(), name='film-list-create'),
-    path('films/<int:pk>/', FilmRetrieveUpdateDeleteView.as_view(), name='film-detail'),
+    path('', include(router.urls)),
 ]
